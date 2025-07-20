3f85a20cb4d1fcaf76ac6d52ab6df107bb021220
Samie Saheb <shs.priv@gmail.com>
2025-07-20 18:37:55 +0700

    Add @mention system with autocomplete, styling, and notifications

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,9 +3,14 @@ class Notification < ApplicationRecord
   belongs_to :recipient, polymorphic: true
   belongs_to :notifiable, polymorphic: true
   belongs_to :actor, class_name: "User", optional: true
+
   scope :unread, -> { where(read_at: nil) }
 
   def mark_as_read!
     update(read_at: Time.current)
+  end
+
+  def message
+    "#{actor.username} #{action} in your #{notifiable_type.downcase}."
   end
 end
